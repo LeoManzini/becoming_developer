4d32c2881ba5c65d03b7b6e9a0ea723fc023e0f8
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-02-09 18:33:03 -0300

    Adding the java for file

---
+++ control_structures/src/br/com/leomanzini/control_structures/For.java
@@ -0,0 +1,25 @@
+package br.com.leomanzini.control_structures;
+
+public class For {
+	
+	public static void main(String args[]) {
+		
+		int counter = 0;
+		
+		// Don't stick to the syntax for now, understand the repetition loop!!
+		
+		  // counter   ; stop condition ; increment
+		for(counter = 1; counter < 10; counter++) {
+			System.out.println(counter);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
