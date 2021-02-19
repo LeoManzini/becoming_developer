e72074b313dc8247dbdd6a1ca4c266f7bb93ea3e
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-02-19 18:53:49 -0300

    Modifying the DoWhile program

--- control_structures/src/br/com/leomanzini/control_structures/DoWhile.java
+++ control_structures/src/br/com/leomanzini/control_structures/DoWhile.java
@@ -5,6 +5,18 @@ public class DoWhile {
 	public static void main(String args[]) {
 		
 		// Don't stick to the syntax for now, understand the repetition loop!!
-		//TODO
+		
+		int counter = 1;
+		 
+		// Loop to print ten numbers
+		do {
+			System.out.println("Current number: " + counter);
+			counter++;
+		} while(counter < 10);  
+
+		 // Starting with false and executing once a time
+		 do {  
+			 System.out.println("Doing what While loop can't!");  
+		 } while(false);
 	}
 }
